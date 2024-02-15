13873b87de2b301257e0559c929b3cc45bb1facc
ZUD0 <anaghbasak@gmail.com>
2024-02-15 19:44:51 +0530

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,45 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate= useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password || !formData.email)
+      return setErrorMessage("Please fill out all the fields");
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -17,28 +54,47 @@ export default function SignUp() {
           <p className="text-sm mt-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. At culpa
             officiis ut ab quaerat molestiae ad earum aut officia a, aliquid
-            veniam necessitatibus illo, laboriosam iusto totam corporis dicta
-            vel?
           </p>
         </div>
 
         {/* RIGHT */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value=" Your UserName" />
-              <TextInput type="text" placeholder="UserName" id="username" />
+              <TextInput
+                type="text"
+                placeholder="UserName"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value=" Your Email" />
-              <TextInput type="text" placeholder="name@company.com" id="email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value=" Your Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
             <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+              {
+              loading?(
+                <>
+                <Spinner size='sm'/>
+                <span style className="pl-3">loading...</span>
+                </>
+              ):'Sign Up'}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -47,6 +103,13 @@ export default function SignUp() {
               Sing In
             </Link>
           </div>
+          {
+            (errorMessage != null) && (
+              <Alert className="mt-5" color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
