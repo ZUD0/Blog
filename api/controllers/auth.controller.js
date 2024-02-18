f72d0adc74f4ff64dd1be9bf2d9e02fb9b3e119e
ZUD0 <anaghbasak@gmail.com>
2024-02-18 19:24:21 +0530

    make the dashboard private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
       const { password, ...rest } = user._doc;
-      rest
+      res
         .status(200)
         .cookie("access_token", token, {
           httpOnly: true,
