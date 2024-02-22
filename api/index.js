b496b29f6fb7231e8d71dda78cedb35cd968a4c2
ZUD0 <anaghbasak@gmail.com>
2024-02-22 20:07:16 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
+import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -25,13 +26,14 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Inter Server Error';
+  const message = err.message || "Inter Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-  })
-})
+  });
+});
